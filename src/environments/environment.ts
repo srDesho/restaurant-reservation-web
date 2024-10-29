cd611cbf67de24f2e91c105f1307195e8e8f7ca5
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-10-28 22:25:23 -0400

    environments layer created

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+   
+    HOST: 'http://localhost:8080/api/v1',  
+    paypalReturnUrl: 'http://localhost:4200/pages/reservation/form'
+
+    // HOST: 'https://restaurant-reservation-api-kv05.onrender.com/api/v1',
+    // paypalReturnUrl: 'https://.netlify.app/pages/reservation/form'
+    
+};
