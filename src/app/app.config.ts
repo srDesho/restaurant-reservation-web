35c90bf430a38b22430fe58fa1de44b318964118
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-10-29 22:29:00 -0400

    adding routes and config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './shared/interceptors/jwt.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NativeDateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideHttpClient(withInterceptors([jwtInterceptor])), 
+    { provide: NativeDateAdapter, useClass: NativeDateAdapter },
+    provideAnimationsAsync(),
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './pages/auth/layout/layout.component';
+import { authGuard } from './shared/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '',  redirectTo: 'auth/sign-in', pathMatch: 'full' },
+	{
+		path: 'auth',		
+		loadComponent: () => import('./pages/auth/layout/layout.component').then((x) => x.LayoutComponent),
+		loadChildren: () =>
+			import('./pages/pages.routes').then((x) => x.pagesRoutes),
+
+		
+	},
+	{
+		path: 'pages',
+		loadComponent: () => import('./pages/home/layout/layout.component').then((x) => x.LayoutComponent),
+		loadChildren: () =>
+		  import('./pages/pages.routes').then((x) => x.pagesRoutes),
+	    canActivate: [authGuard]  
+	},
+];
+
